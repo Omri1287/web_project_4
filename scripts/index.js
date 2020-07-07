ddec3a57cd0aa664e9004b55a21785a815071a47
omri resh <omrsh08@gmail.com>
2020-07-08 02:28:53 +0300

    update 8.7

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,11 @@ const editButton = document.querySelector('.profile__edit-button');
 const closeButton = document.querySelector('.modal__close-btn');
 const modal = document.querySelector('.modal');
 const form = document.querySelector('.modal__form');
+const inputName = document.querySelector('.modal__input_type_name');
+const inputDesc = document.querySelector('.modal__input_type_desc');
+
+const profileName = document.querySelector('.profile__text_type_name');
+const profileDesc = document.querySelector('.profile__text_type_desc');
 
 function toggleModal(){
     modal.classList.toggle('modal_is-open');
@@ -13,12 +18,6 @@ closeButton.addEventListener('click', toggleModal);
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-
-    const inputName = document.querySelector('.modal__input_type_name');
-    const inputDesc = document.querySelector('.modal__input_type_desc');
-
-    const profileName = document.querySelector('.profile__text_type_name');
-    const profileDesc = document.querySelector('.profile__text_type_desc');
 
     profileName.textContent = inputName.value;
     profileDesc.textContent = inputDesc.value;
